a6a21390d9e835a4fce12093d35f008cd3f326fa
hirwa honore <mihigohonore@gmail.com>
2025-07-30 05:09:56 +0200

    my chnages

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,10 @@ import { PrismaModule } from './prisma/prisma.module';
 import { AdminModule } from './modules/admin/admin.module';
 import { EmployeeManagmentModule } from './modules/employee-managment/employee-managment.module';
 import { TaskManagementModule } from './modules/task-management/task-management.module';
+import { CategoryManagementModule } from './modules/category-management/category-management.module';
 
 @Module({
-  imports: [PrismaModule, AdminModule, EmployeeManagmentModule, TaskManagementModule],
+  imports: [PrismaModule, AdminModule, EmployeeManagmentModule, TaskManagementModule, CategoryManagementModule],
   controllers: [AppController],
   providers: [],
 })
---
+++ backend/src/modules/category-management/category-management.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { CategoryManagementService } from './category-management.service';
+import { CategoryManagementController } from './category-management.controller';
+
+@Module({
+  providers: [CategoryManagementService],
+  controllers: [CategoryManagementController]
+})
+export class CategoryManagementModule {}
