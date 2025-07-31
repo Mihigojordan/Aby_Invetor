11fee333718949cfd409af21c96af7b51dce7b5b
12th-ai <mihigojordan8@gmail.com>
2025-07-31 12:04:16 +0200

    hello

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { PrismaModule } from './prisma/prisma.module';
-import { AdminModule } from './modules/admin/admin.module';
-import { EmployeeManagmentModule } from './modules/employee-managment/employee-managment.module';
-import { TaskManagementModule } from './modules/task-management/task-management.module';
-import { CategoryManagementModule } from './modules/category-management/category-management.module';
-import { ProductManagmentModule } from './modules/product-managment/product-managment.module';
-import { StockinManagmentModule } from './modules/stockin-managment/stockin-managment.module';
+import { AdminModule } from './Modules/admin/admin.module';
+import { EmployeeManagmentModule } from './Modules/employee-managment/employee-managment.module';
+import { TaskManagementModule } from './Modules/task-management/task-management.module';
+import { CategoryManagementModule } from './Modules/category-management/category-management.module';
+import { ProductManagmentModule } from './Modules/product-managment/product-managment.module';
+import { StockinManagmentModule } from './Modules/stockin-managment/stockin-managment.module';
 
 @Module({
   imports: [PrismaModule, AdminModule, EmployeeManagmentModule, TaskManagementModule, CategoryManagementModule, ProductManagmentModule, StockinManagmentModule],
