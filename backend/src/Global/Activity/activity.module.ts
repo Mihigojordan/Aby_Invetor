a42f3a0affd4170803f2e193b9bf44b61181454c
serge <125203998+serge740@users.noreply.github.com>
2025-08-12 11:29:08 +0300

    added new changes

--- backend/src/Global/Activity/activity.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ActivityService } from './activity.service';
-
-@Module({
-  providers: [ActivityService],
-  exports: [ActivityService], // So it can be injected in other modules
-})
-export class ActivityModule {}
