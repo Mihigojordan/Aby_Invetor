2303c26136cc447ac5d94ad42b1c98378c456839
serge <125203998+serge740@users.noreply.github.com>
2025-08-16 16:54:02 +0300

    finished printing

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-f84911c5'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.4h0j42f9vkg"
+    "revision": "0.6mi04e58flo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
