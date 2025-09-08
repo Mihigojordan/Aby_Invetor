f420d6b18741318075032197428335f157931e7d
serge <125203998+serge740@users.noreply.github.com>
2025-09-08 10:33:20 +0300

    added new chamge

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.f9hjo7j3o7g"
+    "revision": "0.5ut6tc6l2p"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
